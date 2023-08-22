f510ba152994f78cca81a4a390cb57a696c936f9
Michael Bestas <mkbestas@lineageos.org>
2023-08-22 14:33:06 +0300

    ingot: Don't build unused thermal libs
    Change-Id: Ic09d2d04e71cf03b3c19b696d21778aba28ae6b0

--- device.mk
+++ device.mk
@@ -471,8 +471,7 @@ PRODUCT_COPY_FILES += \
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.qti-v2 \
-    android.hardware.thermal@2.0 \
-    android.hardware.thermal@2.0.vendor
+    android.hardware.thermal@2.0
 
 # Trusted UI
 PRODUCT_PACKAGES += \
