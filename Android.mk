ffc41127f6b98e368d17460b2d8138fd0a7308ee
Michael Bestas <mkbestas@lineageos.org>
2023-08-24 05:12:01 +0300

    ingot: Create libwfdnative symlink for WfdService
    Change-Id: I668d899eabcc9c8f1018a51761b0f566839daca6

--- Android.mk
+++ Android.mk
@@ -121,6 +121,14 @@ $(IMS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 
 ALL_DEFAULT_INSTALLED_MODULES += $(IMS_SYMLINKS)
 
+WFD_SERVICE_SYMLINKS := $(TARGET_OUT_SYSTEM_EXT_APPS_PRIVILEGED)/WfdService/lib/arm64
+$(WFD_SERVICE_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "Creating WfdService symlinks: $@"
+	@mkdir -p $@
+	$(hide) ln -sf /system_ext/lib64/libwfdnative.so $@/libwfdnative.so
+
+ALL_DEFAULT_INSTALLED_MODULES += $(WFD_SERVICE_SYMLINKS)
+
 CNE_APP_SYMLINKS := $(TARGET_OUT_VENDOR)/app/CneApp/lib/arm64
 $(CNE_APP_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "Creating CneApp symlinks: $@"
