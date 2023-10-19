26f3d58510b3ed41cacef1087df25d351dd34cba
SGCMarkus <markusornik@gmail.com>
2023-10-19 11:53:28 +0200

    ingot: Add security.rkp-V3 dependency to keymint service
    The symbol _ZN4aidl7android8hardware8security7keymint29IRemotelyProvisionedComponent10descriptorE
    was moved from android.hardware.security.keymint-V2-ndk to android.hardware.security.rkp-V3-ndk.
    
    Change-Id: I7f3c40e3e8a2a848cdcb73e5232952f63681aa58

--- extract-files.sh
+++ extract-files.sh
@@ -53,6 +53,14 @@ if [ -z "${SRC}" ]; then
     SRC="adb"
 fi
 
+function blob_fixup() {
+    case "${1}" in
+        vendor/bin/hw/android.hardware.security.keymint-service-qti)
+            ${PATCHELF} --add-needed "android.hardware.security.rkp-V3-ndk.so" "${2}"
+            ;;
+    esac
+}
+
 # Initialize the helper
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
 
