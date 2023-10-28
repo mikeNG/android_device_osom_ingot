24f2a0099d9effc2a41a82de677f11bd3187c1ba
Michael Bestas <mkbestas@lineageos.org>
2023-10-28 13:20:58 +0300

    ingot: Update USB gadget HAL to version 1.2
    Change-Id: I0588aff39d9dbd48f5d1447f21f70542ac414f87

--- device.mk
+++ device.mk
@@ -476,7 +476,7 @@ PRODUCT_PACKAGES_DEBUG += \
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service-qti \
-    android.hardware.usb.gadget@1.1-service-qti \
+    android.hardware.usb.gadget@1.2-service-qti \
     init.qcom.usb.rc \
     init.qcom.usb.sh \
     usb_compositions.conf
